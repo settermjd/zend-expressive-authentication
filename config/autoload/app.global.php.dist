4c2a1b777db3d35266f467cbca924e3d4d318afc
Matthew Setter <matthew@matthewsetter.com>
2016-10-17 07:38:43 +0200

    Added a ConfigProvider class for the App namespace
    The ConfigProvider class makes it possible to keep a self-contained
    configuration for a given namespace. For more details about them, refer
    to http://www.masterzendframework.com/configproviders-classes/.

---
+++ config/autoload/app.global.php.dist
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This is a sample configuration file which invokes the ConfigProvider class
+ * for the App namespace. To use it, copy it from here to your application's
+ * `config/autoload` directory, renaming it to `app.global.php`. To learn more
+ * about ConfigProvider classes, check out:
+ * http://www.masterzendframework.com/configproviders-classes/.
+ */
+use App\ConfigProvider;
+
+return (new ConfigProvider())->__invoke();
---
+++ src/App/ConfigProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App;
+
+use App\Action\LoginPageAction;
+use App\Action\LoginPageFactory;
+use App\Middleware\AuthenticationMiddleware;
+use App\Middleware\AuthenticationMiddlewareFactory;
+use App\Repository\UserAuthenticationFactory;
+use App\Repository\UserAuthenticationInterface;
+use App\Repository\UserTableAuthentication;
+
+/**
+ * Class ConfigProvider
+ * @package App
+ */
+class ConfigProvider
+{
+    /**
+     * Provide the configuration for the module
+     *
+     * This class handles the setup of the configuration for this module. This
+     * configuration will be merged into the wider application configuration if
+     * it's enabled.
+     *
+     * @return array
+     */
+    public function __invoke()
+    {
+        return [
+            'dependencies' => $this->getDependencyConfig(),
+            'routes' => $this->getRouteConfig(),
+        ];
+    }
+
+    /**
+     * Provides the namespace's route configuration
+     *
+     * @return array
+     */
+    public function getRouteConfig()
+    {
+        return [
+            [
+                'name' => 'login',
+                'path' => '/login',
+                'middleware' => LoginPageAction::class,
+                'allowed_methods' => ['GET', 'POST'],
+            ],
+        ];
+    }
+
+    /**
+     * Provides the namespace's dependency configuration
+     *
+     * @return array
+     */
+    public function getDependencyConfig()
+    {
+        return [
+            'factories' => [
+                AuthenticationMiddleware::class => AuthenticationMiddlewareFactory::class,
+                LoginPageAction::class => LoginPageFactory::class,
+                /**
+                 * Register a class that will handle the user authentication.
+                 * The one registered here provides only a generic sample implementation
+                 * and is not meant to be taken seriously.
+                 *
+                 * UserTableAuthentication::class => UserAuthenticationFactory::class,
+                 * */
+            ],
+            'aliases' => [
+                /**
+                 * This is a sample setup whereby the specific implementation is never
+                 * referenced anywhere in the codebase, instead using a generic alias.
+                 *
+                 * UserAuthenticationInterface::class => UserTableAuthentication::class
+                 * */
+            ]
+        ];
+    }
+}
